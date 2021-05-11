4847993a087ff890e3547ee0cb2bf6aea2c66030
Dorothee Wallois <dorothee.wallois@live.fr>
2021-05-11 20:39:26 +0200

    first try

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user
   belongs_to :asset
-  has_many :transactions
+  has_many :transactions, class_name: "Transaction", foreign_key: :sending_address
+  self.primary_key = 'address_sequence' 
 
   validates_uniqueness_of :address_sequence
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,3 @@
 class Transaction < ApplicationRecord
-  belongs_to :address
+  belongs_to :sending_address, class_name: "Address", primary_key: 'address_sequence' 
 end
