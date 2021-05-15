c50d5d05b350d2ff7d24a660ef5e3cb7d2ca152c
Tom Benner <tombenner@web.de>
2021-05-15 14:37:02 +0200

    Created Transactions model and some asset seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :addresses, only: [:index,:show] do 
-    resources :transactions, only: [:new, :create]  
-  end  
 
-  resources :transactions, only: [:index]  
+  resources :addresses, only: [:index,:show] do
+    resources :transactions, only: [:new, :create]
+  end
 
-  resources :assets, only: [:index, :show] do 
+  resources :transactions, only: [:index]
+
+  get "/transactions/exchange", to: "transactions#exchange", as: :exchange
+
+  resources :assets, only: [:index, :show] do
     resources :addresses, only: [:new, :create]
   end
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
