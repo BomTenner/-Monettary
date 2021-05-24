3d6f89b93352f376a1b03126193814290d254f92
Tom Benner <tombenner@web.de>
2021-05-24 13:06:57 +0200

    Fixed js issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("plugins/unlock")
 
-require("plugins/transfer")
-
-//require("plugins/homepage")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
