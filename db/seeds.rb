7063531d46f0ad8c7ae4726b26c4194113c39c49
Dorothee Wallois <dorothee.wallois@live.fr>
2021-05-29 02:03:06 +0200

    bg job & historical data pulled from the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 require 'json'
 require 'open-uri'
 
-puts "Cleaning Address Databse"
-Address.destroy_all
+# puts "Cleaning Address Databse"
+# Address.destroy_all
 
-puts "Cleaning Asset Databse"
-Asset.destroy_all
+# puts "Cleaning Asset Databse"
+# Asset.destroy_all
 
-puts "Finished cleaning"
-
-
-all_coins = ["bitcoin","ethereum", "chainlink", "cardano", "dogecoin", "tezos", "polkadot", "uniswap", "litecoin", "solana", "stellar", "vechain"]
-
-all_coins.each do |id|
-  url = "https://api.coingecko.com/api/v3/coins/#{id}"
-  coin_serialized = URI.open(url).read
-  coin = JSON.parse(coin_serialized)
-  puts "Seeding coin: #{coin["name"]}"
-  Asset.create!([{name: coin["name"], price: coin["market_data"]["current_price"]["eur"], ticker: coin["symbol"], description: coin["description"]["en"], logo: coin["image"]["large"]}])
-end
+# puts "Finished cleaning"
 
 
-puts "Finished seeding assets"
+# all_coins = ["bitcoin","ethereum", "chainlink", "cardano", "dogecoin", "tezos", "polkadot", "uniswap", "litecoin", "solana", "stellar", "vechain"]
+
+# all_coins.each do |id|
+#   url = "https://api.coingecko.com/api/v3/coins/#{id}"
+#   coin_serialized = URI.open(url).read
+#   coin = JSON.parse(coin_serialized)
+#   puts "Seeding coin: #{coin["name"]}"
+#   Asset.create!([{name: coin["name"], price: coin["market_data"]["current_price"]["eur"], ticker: coin["symbol"], description: coin["description"]["en"], logo: coin["image"]["large"]}])
+# end
+
+
+# puts "Finished seeding assets"
 
 puts "Cleaning AssetValue Databse"
 AssetValue.destroy_all
