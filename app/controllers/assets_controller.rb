03405074a37c6c79819e432851df149ff4f21083
Ludumo Toni <ludumo.toni@outlook.com>
2021-05-29 14:59:10 +0200

    asset show page changes

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,18 @@
 class AssetsController < ApplicationController
+
   def index
     @assets = Asset.all
   end
+
+  def new
+    @assets = Asset.all
+    @assets.each do |asset|
+      @asset_hash[asset.id] = { name: asset.name, ticker: asset.ticker }
+    end
+  end
+
+  def create
+
+  end
+
 end
