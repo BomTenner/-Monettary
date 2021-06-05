9f71ef6a3b2015fc8304780223f755bdec092b07
Tom Benner <tombenner@web.de>
2021-06-05 11:57:43 +0200

    finished the conversion

--- app/javascript/components/transfer.js
+++ app/javascript/components/transfer.js
@@ -111,10 +111,44 @@ const initTransfer = () => {
       transactionsCompleted.classList.remove("d-none");
     });
   }
-
-
-
 }
 
-export { initTransfer }
+const initConvertAmount = () => {
+  const selectAllButton = document.querySelector('.select-balance');
+  const amountUSD = document.querySelector('.amount-usd');
+  const assetInstance = document.querySelector('.asset-instance');
+  const walletBalance = document.getElementById('current-balance');
+  const amountInputField = document.querySelector('#transaction_sending_amount');
+  if (selectAllButton) {
+    selectAllButton.addEventListener("click", (event) => {
+    const asset = JSON.parse(assetInstance.innerHTML);
+    const usd = walletBalance.innerHTML * asset.price;
+    amountUSD.innerHTML = `${usd}`;
+   });
+  }
+  if (amountInputField) {
+    amountInputField.addEventListener("keyup", (event) => {
+      const asset = JSON.parse(assetInstance.innerHTML);
+      const usd = amountInputField.value * asset.price;
+      amountUSD.innerHTML = `${usd}`;
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { initTransfer, initConvertAmount }
 
